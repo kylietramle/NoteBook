c7a7351149dcea00830c441f87f1340108e83cb7
Kylie Le <kylie.tram.le@gmail.com>
2016-09-22 12:04:04 -0700

    Initial Commit

---
+++ NoteBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NoteBook
+//
+//  Created by Kylie Tram Le on 9/22/16.
+//  Copyright © 2016 Kylie Tram Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
